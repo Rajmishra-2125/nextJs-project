533b26503b59ba93043b582e041bc1a5c60b8aa3
Raj Mishra <raj212558@gmail.com>
2026-02-24 13:07:13 +0530

    Update signup route

--- Projects/my-app/src/app/api/users/signup/route.ts
+++ Projects/my-app/src/app/api/users/signup/route.ts
@@ -1,4 +1,5 @@
 import { connect } from "@/dbconfig/dbConfig";
+import { sendEmail } from "@/helpers/mailer";
 import User from "@/models/userModels";
 import bcryptjs from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
@@ -27,7 +28,16 @@ export async function POST(request: NextRequest) {
       password: hashedPassword,
     });
 
-    await newUser.save();
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+
+    // Send verification email
+    await sendEmail({
+      email,
+      emailType: "VERIFY",
+      userId: savedUser._id,
+    });
+    
 
     return NextResponse.json(
       { 
